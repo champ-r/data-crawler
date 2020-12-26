407095e83988827072d554eb8ec9e8a229ea896d
Al <cangzan@gmail.com>
2020-12-26 11:14:46 +0800

    fix(cron): command.

--- cron.sh
+++ cron.sh
@@ -3,14 +3,13 @@
 go=$(which go)
 npm=$(which npm)
 
-go build .
+$go build .
 ./data-crawler
 cp output/index.json output/op.gg/
 cp output/index.json output/murderbridge/
 
 cd output/op.gg
-npm publish --access public
+$npm publish --access public
 
 cd ../murderbridge
-npm publish --access public
-
+$npm publish --access public
