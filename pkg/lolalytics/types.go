f3ac36a6ff0147c3b7afaa3335bd728af3bd2765
al <cangzan@gmail.com>
2021-05-15 08:49:17 +0800

    fix(lolalytics): correct types.

--- pkg/lolalytics/types.go
+++ pkg/lolalytics/types.go
@@ -4,11 +4,54 @@ type ITierList struct {
 	Cid map[string]interface{} `json:"cid"`
 }
 
+type IItems struct {
+	Start struct {
+		N   float64 `json:"n"`
+		Wr  float64 `json:"wr"`
+		Set []int   `json:"set"`
+	} `json:"start"`
+	Core struct {
+		Set []int   `json:"set"`
+		Wr  float64 `json:"wr"`
+		N   float64 `json:"n"`
+	} `json:"core"`
+	Item4 []struct {
+		ID int     `json:"id"`
+		N  float64 `json:"n"`
+		Wr float64 `json:"wr"`
+	} `json:"item4"`
+	Item5 []struct {
+		ID int     `json:"id"`
+		N  float64 `json:"n"`
+		Wr float64 `json:"wr"`
+	} `json:"item5"`
+	Item6 []struct {
+		ID int     `json:"id"`
+		N  float64 `json:"n"`
+		Wr float64 `json:"wr"`
+	} `json:"item6"`
+}
+
+type IGraphDataItem struct {
+	All         []float64 `json:"all"`
+	DiamondPlus []float64 `json:"diamond_plus"`
+	Platinum    []float64 `json:"platinum"`
+	Gold        []float64 `json:"gold"`
+	Silver      []float64 `json:"silver"`
+	Bronze      []float64 `json:"bronze"`
+	Iron        []float64 `json:"iron"`
+}
+
+type IObjectiveItem struct {
+	Win  []float64 `json:"win"`
+	Lose []float64 `json:"lose"`
+}
+
 type IChampionData struct {
 	Header struct {
-		N           int    `json:"n"`
-		DefaultLane string `json:"defaultLane"`
-		Lane        string `json:"lane"`
+		N           float64 `json:"n"`
+		DefaultLane string  `json:"defaultLane"`
+		Lane        string  `json:"lane"`
 		Counters    struct {
 			Strong []int `json:"strong"`
 			Weak   []int `json:"weak"`
@@ -17,7 +60,7 @@ type IChampionData struct {
 		Pr        float64 `json:"pr"`
 		Br        float64 `json:"br"`
 		Rank      int     `json:"rank"`
-		RankTotal int     `json:"rankTotal"`
+		RankTotal float64 `json:"rankTotal"`
 		Tier      string  `json:"tier"`
 		TopWin    float64 `json:"topWin"`
 		TopElo    string  `json:"topElo"`
@@ -31,36 +74,36 @@ type IChampionData struct {
 		Skillpriority struct {
 			Win struct {
 				ID string  `json:"id"`
-				N  int     `json:"n"`
+				N  float64 `json:"n"`
 				Wr float64 `json:"wr"`
 			} `json:"win"`
 			Pick struct {
 				ID string  `json:"id"`
-				N  int     `json:"n"`
+				N  float64 `json:"n"`
 				Wr float64 `json:"wr"`
 			} `json:"pick"`
 		} `json:"skillpriority"`
 		Skillorder struct {
 			Win struct {
 				ID int64   `json:"id"`
-				N  int     `json:"n"`
+				N  float64 `json:"n"`
 				Wr float64 `json:"wr"`
 			} `json:"win"`
 			Pick struct {
 				ID int64   `json:"id"`
-				N  int     `json:"n"`
+				N  float64 `json:"n"`
 				Wr float64 `json:"wr"`
 			} `json:"pick"`
 		} `json:"skillorder"`
 		Sum struct {
 			Pick struct {
 				ID string  `json:"id"`
-				N  int     `json:"n"`
+				N  float64 `json:"n"`
 				Wr float64 `json:"wr"`
 			} `json:"pick"`
 			Win struct {
 				ID string  `json:"id"`
-				N  int     `json:"n"`
+				N  float64 `json:"n"`
 				Wr float64 `json:"wr"`
 			} `json:"win"`
 		} `json:"sum"`
@@ -68,7 +111,7 @@ type IChampionData struct {
 		Runes struct {
 			Pick struct {
 				Wr   float64 `json:"wr"`
-				N    int     `json:"n"`
+				N    float64 `json:"n"`
 				Page struct {
 					Pri int `json:"pri"`
 					Sec int `json:"sec"`
@@ -81,7 +124,7 @@ type IChampionData struct {
 			} `json:"pick"`
 			Win struct {
 				Wr   float64 `json:"wr"`
-				N    int     `json:"n"`
+				N    float64 `json:"n"`
 				Page struct {
 					Pri int `json:"pri"`
 					Sec int `json:"sec"`
@@ -94,109 +137,17 @@ type IChampionData struct {
 			} `json:"win"`
 		} `json:"runes"`
 		Items struct {
-			Win struct {
-				Start struct {
-					N   int     `json:"n"`
-					Wr  float64 `json:"wr"`
-					Set []int   `json:"set"`
-				} `json:"start"`
-				Core struct {
-					Set []int   `json:"set"`
-					Wr  float64 `json:"wr"`
-					N   int     `json:"n"`
-				} `json:"core"`
-				Item4 []struct {
-					ID int     `json:"id"`
-					N  int     `json:"n"`
-					Wr float64 `json:"wr"`
-				} `json:"item4"`
-				Item5 []struct {
-					ID int     `json:"id"`
-					N  int     `json:"n"`
-					Wr float64 `json:"wr"`
-				} `json:"item5"`
-				Item6 []struct {
-					ID int     `json:"id"`
-					N  int     `json:"n"`
-					Wr float64 `json:"wr"`
-				} `json:"item6"`
-			} `json:"win"`
-			Pick struct {
-				Start struct {
-					N   int     `json:"n"`
-					Wr  float64 `json:"wr"`
-					Set []int   `json:"set"`
-				} `json:"start"`
-				Core struct {
-					Set []int   `json:"set"`
-					Wr  float64 `json:"wr"`
-					N   int     `json:"n"`
-				} `json:"core"`
-				Item4 []struct {
-					ID int     `json:"id"`
-					N  int     `json:"n"`
-					Wr float64 `json:"wr"`
-				} `json:"item4"`
-				Item5 []struct {
-					ID int     `json:"id"`
-					N  int     `json:"n"`
-					Wr float64 `json:"wr"`
-				} `json:"item5"`
-				Item6 []struct {
-					ID int     `json:"id"`
-					N  int     `json:"n"`
-					Wr float64 `json:"wr"`
-				} `json:"item6"`
-			} `json:"pick"`
+			Win  IItems `json:"win"`
+			Pick IItems `json:"pick"`
 		} `json:"items"`
 	} `json:"summary"`
 	Graph struct {
-		Dates []string `json:"dates"`
-		Wr    struct {
-			All         []float64 `json:"all"`
-			DiamondPlus []float64 `json:"diamond_plus"`
-			Platinum    []float64 `json:"platinum"`
-			Gold        []float64 `json:"gold"`
-			Silver      []float64 `json:"silver"`
-			Bronze      []float64 `json:"bronze"`
-			Iron        []float64 `json:"iron"`
-		} `json:"wr"`
-		Wrs struct {
-			All         []float64 `json:"all"`
-			DiamondPlus []float64 `json:"diamond_plus"`
-			Platinum    []float64 `json:"platinum"`
-			Gold        []float64 `json:"gold"`
-			Silver      []float64 `json:"silver"`
-			Bronze      []float64 `json:"bronze"`
-			Iron        []float64 `json:"iron"`
-		} `json:"wrs"`
-		Pr struct {
-			All         []float64 `json:"all"`
-			DiamondPlus []float64 `json:"diamond_plus"`
-			Platinum    []int     `json:"platinum"`
-			Gold        []float64 `json:"gold"`
-			Silver      []float64 `json:"silver"`
-			Bronze      []float64 `json:"bronze"`
-			Iron        []float64 `json:"iron"`
-		} `json:"pr"`
-		N struct {
-			All         []int `json:"all"`
-			DiamondPlus []int `json:"diamond_plus"`
-			Platinum    []int `json:"platinum"`
-			Gold        []int `json:"gold"`
-			Silver      []int `json:"silver"`
-			Bronze      []int `json:"bronze"`
-			Iron        []int `json:"iron"`
-		} `json:"n"`
-		Br struct {
-			All         []float64 `json:"all"`
-			DiamondPlus []float64 `json:"diamond_plus"`
-			Platinum    []float64 `json:"platinum"`
-			Gold        []float64 `json:"gold"`
-			Silver      []float64 `json:"silver"`
-			Bronze      []float64 `json:"bronze"`
-			Iron        []float64 `json:"iron"`
-		} `json:"br"`
+		Dates []string       `json:"dates"`
+		Wr    IGraphDataItem `json:"wr"`
+		Wrs   IGraphDataItem `json:"wrs"`
+		Pr    IGraphDataItem `json:"pr"`
+		N     IGraphDataItem `json:"n"`
+		Br    IGraphDataItem `json:"br"`
 	} `json:"graph"`
 	Nav struct {
 		Lanes struct {
@@ -207,52 +158,52 @@ type IChampionData struct {
 			Support float64 `json:"support"`
 		} `json:"lanes"`
 	} `json:"nav"`
-	Analysed   int             `json:"analysed"`
+	Analysed   float64         `json:"analysed"`
 	AvgWinRate float64         `json:"avgWinRate"`
 	Top        [][]interface{} `json:"top"`
 	Depth      []interface{}   `json:"depth"`
-	N          int             `json:"n"`
+	N          float64         `json:"n"`
 	Skills     struct {
-		SkillEarly  [][][]int       `json:"skillEarly"`
-		Skill6Pick  int             `json:"skill6Pick"`
-		Skill10Pick int             `json:"skill10Pick"`
+		SkillEarly  [][][]float64   `json:"skillEarly"`
+		Skill6Pick  float64         `json:"skill6Pick"`
+		Skill10Pick float64         `json:"skill10Pick"`
 		SkillOrder  [][]interface{} `json:"skillOrder"`
 	} `json:"skills"`
-	Time map[string]int `json:"time"`
-	TimeWin map[string]int `json:"timeWin"`
+	Time     map[string]float64 `json:"time"`
+	TimeWin  map[string]float64 `json:"timeWin"`
 	TopStats struct {
-		Toppick  int     `json:"toppick"`
-		Toprank  int     `json:"toprank"`
-		Topcount int     `json:"topcount"`
+		Toppick  float64 `json:"toppick"`
+		Toprank  float64 `json:"toprank"`
+		Topcount float64 `json:"topcount"`
 		Topwin   float64 `json:"topwin"`
 		Topelo   string  `json:"topelo"`
 	} `json:"topStats"`
 	Stats      [][]interface{} `json:"stats"`
-	StatsCount int             `json:"statsCount"`
+	StatsCount float64         `json:"statsCount"`
 	Runes      struct {
 		Stats map[string][]float64 `json:"stats"`
 	} `json:"runes"`
-	Objective map[string][]float64 `json:"objective"`
-	Spell    [][]float64         `json:"spell"`
-	Spells   [][]interface{} `json:"spells"`
-	ItemSets struct {
+	Objective map[string]IObjectiveItem `json:"objective"`
+	Spell     [][]float64               `json:"spell"`
+	Spells    [][]interface{}           `json:"spells"`
+	ItemSets  struct {
 		ItemBootSet1 map[string][]int `json:"itemBootSet1"`
 		ItemBootSet2 map[string][]int `json:"itemBootSet2"`
 		ItemBootSet3 map[string][]int `json:"itemBootSet3"`
 	} `json:"itemSets"`
-	StartItem    [][]float64         `json:"startItem"`
+	StartItem    [][]float64     `json:"startItem"`
 	StartSet     [][]interface{} `json:"startSet"`
-	EarlyItem    [][]float64         `json:"earlyItem"`
-	Boots        [][]float64         `json:"boots"`
-	MythicItem   [][]float64         `json:"mythicItem"`
-	PopularItem  [][]float64         `json:"popularItem"`
-	WinningItem  [][]float64         `json:"winningItem"`
-	Item         [][]float64         `json:"item"`
-	Item1        [][]float64         `json:"item1"`
-	Item2        [][]float64         `json:"item2"`
-	Item3        [][]float64         `json:"item3"`
-	Item4        [][]float64         `json:"item4"`
-	Item5        [][]float64         `json:"item5"`
+	EarlyItem    [][]float64     `json:"earlyItem"`
+	Boots        [][]float64     `json:"boots"`
+	MythicItem   [][]float64     `json:"mythicItem"`
+	PopularItem  [][]float64     `json:"popularItem"`
+	WinningItem  [][]float64     `json:"winningItem"`
+	Item         [][]float64     `json:"item"`
+	Item1        [][]float64     `json:"item1"`
+	Item2        [][]float64     `json:"item2"`
+	Item3        [][]float64     `json:"item3"`
+	Item4        [][]float64     `json:"item4"`
+	Item5        [][]float64     `json:"item5"`
 	EnemyTop     [][]float64     `json:"enemy_top"`
 	EnemyJungle  [][]float64     `json:"enemy_jungle"`
 	EnemyMiddle  [][]float64     `json:"enemy_middle"`
